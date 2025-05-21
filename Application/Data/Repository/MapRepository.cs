6f602570f88c7addf91d93023b9be1672fea0b4c
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-05-21 23:52:05 +0200

    Added validation and builder services in program.

--- Application/Data/Repository/MapRepository.cs
+++ Application/Data/Repository/MapRepository.cs
@@ -1,8 +1,9 @@
 ﻿using Application.Data.Context;
 using Application.Domain.Entities;
+using Application.Interfaces;
 
 namespace Application.Data.Repository;
 
-public class MapRepository(DataContext context) : BaseRepository<MapEntity>(context) 
+public class MapRepository(DataContext context) : BaseRepository<MapEntity>(context), IMapRepository
 {
 }
---
+++ Application/External/Services/EventIdValidationService.cs
@@ -0,0 +1,36 @@
+﻿using Application.External.Interfaces;
+using Application.External.Models;
+using Application.External.Response;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Application.External.Services;
+
+public class EventIdValidationService : IEventIdValidationService
+{
+    // Method made with help from chatgpt.
+    private readonly HttpClient _httpClient;
+    private readonly string _apiUrl;
+
+    public EventIdValidationService(HttpClient httpClient, IOptions<EventSettings> options)
+    {
+        _httpClient = httpClient;
+        _apiUrl = options.Value.Url;
+    }
+
+    public async Task<ExternalResponse> EventExistance(string eventId)
+    {
+        var response = await _httpClient.GetAsync(_apiUrl);
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        var events = JsonSerializer.Deserialize<List<EventPoco>>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        if (events == null) { return ExternalResponse.Error("Events returned are null."); }
+        if (!events.Any(e => e.Id.ToString() == eventId)) { return ExternalResponse.NotFound("Event id not found among events."); }
+
+        return ExternalResponse.Ok();
+    }
+}
