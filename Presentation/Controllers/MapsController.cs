a13c5ef92110fbffcc147bb406f420744c1468e1
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-05-29 14:09:45 +0200

    Added api key

--- Presentation/Controllers/MapsController.cs
+++ Presentation/Controllers/MapsController.cs
@@ -5,62 +5,63 @@ using Application.Interfaces;
 using Application.Internal.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Presentation.Extensions.Attributes;
 
-namespace Presentation.Controllers
+namespace Presentation.Controllers;
+
+[UseApiKey]
+[Route("api/[controller]")]
+[ApiController]
+public class MapsController(IMapService mapService, IEventIdValidationService eventValidation) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MapsController(IMapService mapService, IEventIdValidationService eventValidation) : ControllerBase
+    private readonly IMapService _mapService = mapService;
+    private readonly IEventIdValidationService _eventValidation = eventValidation;
+
+
+    [HttpPost]
+    public async Task<IActionResult> CreateMap(AddEventMapForm addForm)
     {
-        private readonly IMapService _mapService = mapService;
-        private readonly IEventIdValidationService _eventValidation = eventValidation;
+        if (!ModelState.IsValid) { return BadRequest("AddEventMapForm is invalid."); }
 
+        var eventExistance = await _eventValidation.EventExistance(addForm.EventId);
+        if (!eventExistance.Success) { return BadRequest("EventId does not exist."); }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateMap(AddEventMapForm addForm)
-        {
-            if (!ModelState.IsValid) { return BadRequest("AddEventMapForm is invalid."); }
+        var result = await _mapService.CreateMapAsync(addForm);
+        if (!result.Success) { return BadRequest(result.Message); }
 
-            var eventExistance = await _eventValidation.EventExistance(addForm.EventId);
-            if (!eventExistance.Success) { return BadRequest("EventId does not exist."); }
+        return Ok(result);
+    }
 
-            var result = await _mapService.CreateMapAsync(addForm);
-            if (!result.Success) { return BadRequest(result.Message); }
+    [HttpPut]
+    public async Task<IActionResult> UpdateMap(UpdateEventMapForm updateForm)
+    {
+        if (!ModelState.IsValid) { return BadRequest("UpdateEventMapForm is invalid."); }
 
-            return Ok(result);
-        }
+        var result = await _mapService.UpdateMapAsync(updateForm);
+        if (!result.Success) { return BadRequest(result.Message); }
 
-        [HttpPut]
-        public async Task<IActionResult> UpdateMap(UpdateEventMapForm updateForm)
-        {
-            if (!ModelState.IsValid) { return BadRequest("UpdateEventMapForm is invalid."); }
+        return Ok(result);
+    }
 
-            var result = await _mapService.UpdateMapAsync(updateForm);
-            if (!result.Success) { return BadRequest(result.Message); }
+    [HttpGet("{eventId}")]
+    public async Task<IActionResult> GetMap(string eventId)
+    {
+        if (eventId == null) { return BadRequest("EventId is null."); }
 
-            return Ok(result);
-        }
+        var result = await _mapService.GetMapAsync(eventId);
+        if (!result.Success) { return BadRequest(result.Message); }
 
-        [HttpGet("{eventId}")]
-        public async Task<IActionResult> GetMap(string eventId)
-        {
-            if (eventId == null) { return BadRequest("EventId is null."); }
+        return Ok(result);
+    }
 
-            var result = await _mapService.GetMapAsync(eventId);
-            if (!result.Success) { return BadRequest(result.Message); }
+    [HttpDelete("{eventId}")]
+    public async Task<IActionResult> DeleteMap(string eventId) 
+    {
+        if (eventId == null) { return BadRequest("EventId given is null."); }
 
-            return Ok(result);
-        }
+        var result = await _mapService.DeleteMapAsync(eventId);
+        if (!result.Success) { return BadRequest(result.Message); }
 
-        [HttpDelete("{eventId}")]
-        public async Task<IActionResult> DeleteMap(string eventId) 
-        {
-            if (eventId == null) { return BadRequest("EventId given is null."); }
-
-            var result = await _mapService.DeleteMapAsync(eventId);
-            if (!result.Success) { return BadRequest(result.Message); }
-
-            return Ok(result);
-        }
+        return Ok(result);
     }
 }
